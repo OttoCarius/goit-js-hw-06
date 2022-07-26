0ba414af586896cf19aad5259811fc1c8a9c1f15
OttoCarius <ottocarius1993@gmail.com>
2022-07-26 15:41:30 +0300

    task 2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const li = [];
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+
+  li.push(item);
+});
+document.querySelector("ul").append(...li);
