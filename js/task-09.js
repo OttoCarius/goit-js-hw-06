71bae3f79193a537a37f95e881af059049a8ecd0
OttoCarius <ottocarius1993@gmail.com>
2022-07-27 18:04:56 +0300

    task9 - done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector(".change-color");
+const text = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const hexColor = getRandomHexColor();
+  text.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
